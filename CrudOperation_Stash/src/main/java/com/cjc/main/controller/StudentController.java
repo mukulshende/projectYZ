87aac5bbdb1c0626e832de2a71537d98ba6a270c
mukulshende <mukulshende20@gmail.com>
2024-07-04 16:30:03 +0530

    commit by mukul

--- CrudOperation_Stash/src/main/java/com/cjc/main/controller/StudentController.java
+++ CrudOperation_Stash/src/main/java/com/cjc/main/controller/StudentController.java
@@ -1,5 +1,18 @@
 package com.cjc.main.controller;
 
-public class StudentController {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cjc.main.service.StudentService;
+
+@RestController
+public class StudentController 
+{
+	@Autowired
+	private StudentService studentService;
+	
+	
+	
 
 }
--- CrudOperation_Stash/src/main/java/com/cjc/main/impl/StudentServiceImpl.java
+++ CrudOperation_Stash/src/main/java/com/cjc/main/impl/StudentServiceImpl.java
@@ -1,10 +1,14 @@
 package com.cjc.main.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cjc.main.repository.StudentRepository;
 import com.cjc.main.service.StudentService;
 
 @Service
 public class StudentServiceImpl implements StudentService{
+	@Autowired
+	private StudentRepository sr;
 
 }
